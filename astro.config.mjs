1806238e0989cca49f534c27435b17e1511ae745
Morten <selex911@gmail.com>
2025-05-06 14:55:34 +0200

    [Guide] Added Linux guide
    Added Linux guide on building SylCore.
    Thank you @minzi90 for writing the guide!

--- astro.config.mjs
+++ astro.config.mjs
@@ -23,17 +23,31 @@ export default defineConfig({
       
       sidebar: [
         {
-          label: 'SylCore Installation',
+          label: 'SylCore Installation [Windows]',
           items: [
-            { label: 'Requirements', link: 'guides/requirements' },
+            { label: 'Requirements', link: 'guides/windows/requirements' },
             //{ label: 'Requirements', link: 'Test-Website/guides/requirements' },
-            { label: 'Installation', link: 'guides/installation' },
+            { label: 'Installation', link: 'guides/windows/installation' },
             //{ label: 'Installation', link: 'Test-Website/guides/installation' },
-            { label: 'Server Setup', link: 'guides/server-setup' },
+            { label: 'Server Setup', link: 'guides/windows/server-setup' },
             //{ label: 'Server Setup', link: 'Test-Website/guides/server-setup' },
-            { label: 'Final Server Steps', link: 'guides/final-server-steps' },
+            { label: 'Final Server Steps', link: 'guides/windows/final-server-steps' },
             //{ label: 'Final Server Steps', link: 'Test-Website/guides/final-server-steps' },
             //{ label: 'Troubleshooting', link: 'guides/troubleshooting' },
+          ],
+        },
+
+        {
+          label: 'SylCore Installation [Linux]',
+          items: [
+            { label: 'Requirements', link: 'guides/linux/requirements' },
+            //{ label: 'Requirements', link: 'Test-Website/guides/requirements' },
+            { label: 'Installation', link: 'guides/linux/installation' },
+            //{ label: 'Server Setup', link: 'Test-Website/guides/server-setup' },
+            { label: 'Final Server Steps', link: 'guides/linux/final-server-steps' },
+
+            { label: 'Optional', link: 'guides/linux/update' },
+            
           ],
         },
       ],
